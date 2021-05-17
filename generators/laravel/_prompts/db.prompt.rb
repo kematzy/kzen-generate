d6a0622282fecd3f402f84a088fa89fd462098de
Kematzy <kematzy@gmail.com>
2021-05-17 12:08:24 +0800

    Improve prompt DB

--- generators/laravel/_prompts/db.prompt.rb
+++ generators/laravel/_prompts/db.prompt.rb
@@ -10,6 +10,13 @@ prompt_start
 # check if we have db.type already defined
 db = confs.fetch('db.type')
 logger.debug("db is =>: #{db.inspect}")
+
+defaults = {
+  sqlite: { name: 'database' },
+  pgsql:  { host: '127.0.0.1', port: '5432', username: 'kzen', password: '123456' },
+  pgsql:  { host: 'localhost', port: '3306', username: 'kzen', password: '' },
+}
+
 # db: can either be:
 #   1) nil (nothing here)
 #   2) a string (sqlite)
@@ -40,24 +47,18 @@ else
 
   if dbtype != 'sqlite'
     # host
-    confs.set('db.host', value: prompt.ask("Add the DB Host", default: '127.0.0.1'))
+    confs.set('db.host', value: prompt.ask("Add the DB Host", default: defaults[dbtype.to_sym][:host]))
     # port
-    confs.set('db.port', value: prompt.ask("Add the DB Port", default: dbtype == 'mysql' ? '3306' : '5432' ))
+    confs.set('db.port', value: prompt.ask("Add the DB Port", default: defaults[dbtype.to_sym][:port]))
     # database
     confs.set('db.database', value: prompt.ask("Add the DB database name", default: confs.fetch(:project_name).gsub('-', '_') + '_dev'))
     # username
-    confs.set('db.username', value: prompt.ask("Add the DB username", default: dbtype == 'mysql' ? 'root' : 'postgres'))
+    confs.set('db.username', value: prompt.ask("Add the DB username", default: defaults[dbtype.to_sym][:username]))
     # password
-    confs.set('db.password', value: prompt.mask("Add the DB password", default: ''))
+    confs.set('db.password', value: prompt.mask("Add the DB password", default: defaults[dbtype.to_sym][:password]))
   end
 end
 logger.debug("set db to: #{confs.fetch(:db)}")
-
-# confs.set('db.type', value: prompt.select("Choose DB setup?", %w(sqlite pgsql mysql), default: 'sqlite')) unless confs.fetch('db.type')
-
-# handle pg & mysql
-# dbtype = confs.fetch('db.type')
-
 
 
 prompt_end
