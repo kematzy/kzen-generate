8c48988d46d62637c63bdfe0831cb9e72a023462
Kematzy <kematzy@gmail.com>
2021-06-23 09:46:54 +0800

    Update vetur.patch.rb

--- generators/laravel/js/vue/vetur.patch.rb
+++ generators/laravel/js/vue/vetur.patch.rb
@@ -7,18 +7,25 @@ set_current_patch 'js:vue:vetur'
 puts
 patch_start
 
+# set the config value if not set
+confs.set('js.vue.vetur', value: true) unless confs.fetch('js.vue.vetur')
 
-contents = <<~VSCODE
-{
-  "include": [
-    "./resources/js/**/*"
-  ]
-}
-VSCODE
-create_file('jsconfig.json', contents, verbose_opts)
-git_commit('add jconfig.json file for Vetur support')
-logger.success 'added jsconfig.json file for Vetur'
+if vue?
 
+  contents = <<~VSCODE
+  {
+    "include": [
+      "./resources/js/**/*"
+    ]
+  }
+  VSCODE
+  create_file('jsconfig.json', contents, verbose_opts)
+  git_commit('add jconfig.json file for Vetur support')
+  logger.success 'added jsconfig.json file for Vetur'
+
+else
+  patch_required_for_feature('js:vue', 'Vue')
+end
 
 patch_end
 puts
