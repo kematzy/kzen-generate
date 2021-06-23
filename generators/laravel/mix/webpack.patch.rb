d9dbcf588a697a1ed89cb3eef9e9176121ddb26a
Kematzy <kematzy@gmail.com>
2021-06-23 09:45:36 +0800

    Update webpack.patch.rb

--- generators/laravel/mix/webpack.patch.rb
+++ generators/laravel/mix/webpack.patch.rb
@@ -13,20 +13,34 @@ puts
 patch_start
 
 
-if confs.fetch('js.type')
+# set the config value if not set
+confs.set('dev.mix.webpack', value: true) unless confs.fetch('dev.mix.webpack')
 
-  template(tmpl_file("mix/#{confs.fetch('js.type')}/webpack.config.js"), 'webpack.config.js')
+if js_configured?
+
+  template(tmpl_file("mix/#{get_js_type}/webpack.config.js"), 'webpack.config.js')
   git_commit('added webpack.config.js file')
   logger.success 'added webpack.config.js file'
 
-  # replace contents in webpack.mix.js
-  newcode = <<-NEW
+# replace contents in webpack.mix.js
+oldcode = <<-FIND
+  // insert
+FIND
+newcode = <<-NEW
   .webpackConfig(require('./webpack.config'))
-      // insert
-  NEW
-  gsub_file('webpack.mix.js', '// insert', newcode, verbose_opts)
+  // .version()
+  // .sourceMaps()
+  // insert
+NEW
+  gsub_file('webpack.mix.js', oldcode, newcode, verbose_opts)
   git_commit('updated webpack.mix.js')
   logger.success 'updated webpack.mix.js file with WebPack configs'
+
+  if svelte?
+    run_patch('js:svelte:loader')
+
+    run_patch('js:svelte:preprocess')
+  end
 
 else
   logger.fatal('mix:webpack: failed to access the js.type setting')
