e7b488fee72adf0f3e535ea7b6907836d59fa1d1
Kematzy <kematzy@gmail.com>
2021-06-23 10:48:19 +0800

    Update Git init patch

--- generators/laravel/git/init.patch.rb
+++ generators/laravel/git/init.patch.rb
@@ -5,9 +5,22 @@ set_current_patch 'git:init'
 # puts
 patch_start
 
+# set the config value if not set
+confs.set('git.enabled', value: true) unless confs.fetch('git.enabled')
 
 run('git init', debug_opts)
 logger.git "created Git repository"
+
+# ignore public directory changes
+contents  = <<~CODE
+
+# temporary exclusions
+public/css/app.css
+public/js/*.js
+public/js/*.txt
+public/mix-manifest.json
+CODE
+append_to_file('.gitignore', contents, debug_opts)
 
 git_commit('initial commit')
 
