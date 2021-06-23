2514713dae987d62563489a2737c3f99c0a67f13
Kematzy <kematzy@gmail.com>
2021-06-23 09:45:24 +0800

    Update browser-sync.patch.rb

--- generators/laravel/mix/browser-sync.patch.rb
+++ generators/laravel/mix/browser-sync.patch.rb
@@ -3,22 +3,24 @@
 # DOCS: https://laravel-mix.com/docs/6.0/browsersync
 #       https://browsersync.io/docs/options/
 
-
 set_current_patch 'mix:browsersync'
 
 puts
 patch_start
 
 
+# set the config value if not set
+confs.set('dev.mix.browsersync', value: true) unless confs.fetch('dev.mix.browsersync')
+
 npm_install_dev('browser-sync browser-sync-webpack-plugin', 'installed browserSync packages')
 
 newcode = <<-NEW
-// insert
-    .browserSync('http://#{@project_name}.test')
+  // insert
+  // .browserSync('http://#{@project_name}.test')
 NEW
 
 # replace contents in webpack.mix.js
-gsub_file('webpack.mix.js', '// insert', newcode, verbose_opts)
+gsub_file('webpack.mix.js', '  // insert', newcode, verbose_opts)
 git_commit('update webpack.mix.js')
 logger.success 'updated webpack.mix.js file with browserSync configs'
 
