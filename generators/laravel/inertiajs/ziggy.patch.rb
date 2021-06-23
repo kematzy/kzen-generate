39e663bbc49e9bf1440e5dfb549e9ebdb10114bd
Kematzy <kematzy@gmail.com>
2021-06-23 09:47:13 +0800

    Update ziggy.patch.rb

--- generators/laravel/inertiajs/ziggy.patch.rb
+++ generators/laravel/inertiajs/ziggy.patch.rb
@@ -9,12 +9,17 @@ set_current_patch 'inertiajs:ziggy'
 puts
 patch_start
 
+if inertiaJS?
 
-composer_install('tightenco/ziggy')
+  composer_install('tightenco/ziggy')
 
-gsub_file('resources/views/app.blade.php', '<!-- Scripts -->', "@routes\n  <!-- Scripts -->", verbose_opts)
-git_commit('added @routes to views/app.blade file')
-logger.success 'added @routes to views/app.blade file'
+  gsub_file('resources/views/app.blade.php', '<!-- Scripts -->', "@routes\n  <!-- Scripts -->", verbose_opts)
+  git_commit('added @routes to views/app.blade file')
+  logger.success 'added @routes to views/app.blade file'
+
+else
+  patch_required_for_feature('js:inertia', 'InertiaJS')
+end
 
 
 patch_end
