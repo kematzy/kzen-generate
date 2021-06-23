63a4012ebef89f536bce82188e50a7bb3cf61842
Kematzy <kematzy@gmail.com>
2021-06-23 09:22:13 +0800

    Update helpers.rb

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -45,11 +45,29 @@ module Kzen
       end
 
 
+      #### FILE HELPERS
+
+      def file_contains?(haystack, needle)
+        File.readlines(File.expand_path(haystack)).grep(/#{needle}/).size > 0
+      end
+
+      def package_installed?(package)
+        res = File.readlines(File.expand_path('package.json')).grep(/"#{package}"/).size > 0
+        if res
+          logger.info("Yes, '#{package}' node package installed")
+          return true
+        else
+          logger.info("No, '#{package}' node package NOT installed")
+          return false
+        end
+      end
+
+
       #### GIT HELPERS
 
       def git?
         # options[:git]
-        confs.fetch('features.git')
+        confs.fetch('git.enabled')
       end
       alias_method :using_git?, :git?
 
@@ -102,6 +120,7 @@ module Kzen
       end
 
       def set_current_patch(p = nil)
+        @previous_patch = @current_patch
         @current_patch = p.nil? ? @patch : p
       end
 
@@ -113,6 +132,25 @@ module Kzen
       def patch_end(custom = nil)
         c = custom.nil? ? @current_patch : custom
         logger.end("patch: #{dc(c)}")
+        @current_patch = @previous_patch
+      end
+
+      def get_current_patch
+        @current_patch
+      end
+
+      def patch_required_for_feature(parent_patch, feature)
+        logger.fatal "Unable to run '#{get_current_patch}' because #{feature} is not configured"
+        logger.info " -- run 'kzen laravel #{parent_patch}' before running this commmand"
+      end
+
+      def patch_recommended(parent_patch, feature)
+        logger.info "Recommendation: Feature #{feature} is not configured"
+        logger.info " -- run 'kzen laravel #{parent_patch}' to install this functionality"
+      end
+
+      def patch_disabled
+        logger.fatal "This patch: '#{get_current_patch}' is disabled or not ready for use"
       end
 
       #### PROMPT HELPERS
@@ -147,6 +185,16 @@ module Kzen
       def prompt_end(custom = nil)
         c = custom.nil? ? @current_prompt : custom
         logger.end("prompt: #{dc(c)}")
+      end
+
+      #### FILE HELPERS
+
+      def replace_in_file(file, replacements, msg, opts={})
+        replacements.each do |r|
+          gsub_file(file, r.find, r.replace, verbose_opts.merge(opts))
+        end
+        git_commit(msg)
+        logger.success(msg)
       end
 
   end
