a092d3763cb68ea60e8873e7fe7998ebd710a346
Kematzy <kematzy@gmail.com>
2021-06-23 10:46:11 +0800

    Update css / stylelint patch

--- generators/laravel/css/stylelint.patch.rb
+++ generators/laravel/css/stylelint.patch.rb
@@ -7,18 +7,28 @@ set_current_patch 'css:stylelint'
 puts
 patch_start
 
+# set the config value if not set
+confs.set('css.stylelint', value: true) unless confs.fetch('css.stylelint')
 
-npm_install_dev('stylelint stylelint-config-standard stylelint-config-recommended')
+npm_install_dev('stylelint')
+
+npm_install_dev('stylelint-config-standard')
+
+npm_install_dev('stylelint-config-recommended')
 
 template(tmpl_file("stylelint/stylelintrc.json"), '.stylelintrc.json', verbose_opts)
 git_commit('added .stylelintrc.json')
 logger.success 'added .stylelintrc.json file'
 
+template(tmpl_file("stylelint/stylelintignore"), '.stylelintignore', verbose_opts)
+git_commit('added .stylelintignore file')
+logger.success 'added .stylelintignore file'
+
 template(tmpl_file("stylelint/vscode-settings.json"), '.vscode/settings.json', verbose_opts)
 git_commit('added .vscode/settings.json')
 logger.success 'added .vscode/settings.json file'
 
-npm_run_dev
+# npm_run_dev
 
 
 patch_end
